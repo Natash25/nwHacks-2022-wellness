8ee2e0410d2a70991ba8501ab5e54d2e81994a02
MangoMelona <shen.leo@outlook.com>
2022-01-15 20:17:41 -0800

    debug card population

--- src/components/Appointments/SingleCard/index.js
+++ src/components/Appointments/SingleCard/index.js
@@ -1,56 +1,60 @@
-import React from 'react'
-import Component from 'react'
-import PropTypes from 'prop-types'
-import { Button, Card, Image, Item } from 'semantic-ui-react'
+import React from "react";
+import Component from "react";
+import PropTypes from "prop-types";
+import { Button, Card, Image, Item } from "semantic-ui-react";
 
-class SingleCard extends Component{
-
-    static propTypes = {
-        name: PropTypes.string,
-        title: PropTypes.string,
-        startTime: PropTypes.string,
-        endTime: PropTypes.string,
-        callLink: PropTypes.string,
-    };
-
-    render() {
-        const {
-            name,
-            title,
-            startTime,
-            endTime,
-            callLink,
-        } = this.props;
-        return (
-            <div>
-            <Card>
-              <Card.Content>
-                <Image
-                  floated='right'
-                  size='mini'
-                  src='/images/avatar/large/steve.jpg'
-                />
-                <Card.Header>{name}</Card.Header>
-                <Card.Meta>{title}</Card.Meta>
-                <Card.Description>{startTime} - {endTime}</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui two buttons'>
-                  <Button basic color='green' class="JoinCall" role="link" href={callLink}>
-                    Join Call
-                  </Button>
-                  <Button basic color='red'>
-                    Cancel
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
+function SingleCard({ name, title, startTime, endTime, callLink }) {
+  return (
+    <div>
+      <Card>
+        <Card.Content>
+          <Image
+            floated="right"
+            size="mini"
+            src="/images/avatar/large/steve.jpg"
+          />
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>{title}</Card.Meta>
+          <Card.Description>
+            {startTime} - {endTime}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui two buttons">
+            <Button
+              basic
+              color="green"
+            //   class="JoinCall"
+              role="link"
+              href={callLink}
+            >
+              Join Call
+            </Button>
+            <Button basic color="red">
+              Cancel
+            </Button>
           </div>
-        );
-    }
-
-
+        </Card.Content>
+      </Card>
+    </div>
+  );
 }
-  
 
-  export default SingleCard;
+SingleCard.displayName = "SingleCard";
+SingleCard.propTypes = {
+  name: PropTypes.string,
+  title: PropTypes.string,
+  startTime: PropTypes.string,
+  endTime: PropTypes.string,
+  callLink: PropTypes.string,
+};
+
+SingleCard.defaultProps = {
+  name: "Full Name",
+  title: "Role",
+  startTime: "12:00",
+  endTime: "11:59",
+  callLink: "wwww.google.com",
+};
+
+export default SingleCard;
