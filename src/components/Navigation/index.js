30f125f46ab0e301790d6cee0e1c691fae7ac18f
OceNick <ozcenick@gmail.com>
2022-01-15 21:14:17 -0800

    Add navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,29 +1,57 @@
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import {Menu} from "semantic-ui-react";
+
 
 const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.APPOINTMENTS}>Appointments</Link>
-      </li>
-    </ul>
-  </div>
+      <Menu>
+      <Menu.Item
+          name='Sign In'
+          href={ROUTES.SIGN_IN}
+      />
+      <Menu.Item
+          name='Landing'
+          href={ROUTES.LANDING}
+      />
+      <Menu.Item
+          name='Home'
+          href={ROUTES.HOME}
+      />
+      <Menu.Item
+          name='Account'
+          href={ROUTES.ACCOUNT}
+      />
+      <Menu.Item
+          name='Admin'
+          href={ROUTES.ADMIN}
+      />
+      <Menu.Item
+          name='Appointments'
+          href={ROUTES.APPOINTMENTS}
+      />
+      </Menu>
+  // <div>
+  //   <ul>
+  //     <li>
+  //       <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+  //     </li>
+  //     <li>
+  //       <Link to={ROUTES.LANDING}>Landing</Link>
+  //     </li>
+  //     <li>
+  //       <Link to={ROUTES.HOME}>Home</Link>
+  //     </li>
+  //     <li>
+  //       <Link to={ROUTES.ACCOUNT}>Account</Link>
+  //     </li>
+  //     <li>
+  //       <Link to={ROUTES.ADMIN}>Admin</Link>
+  //     </li>
+  //     <li>
+  //       <Link to={ROUTES.APPOINTMENTS}>Appointments</Link>
+  //     </li>
+  //   </ul>
+  // </div>
 );
 
 export default Navigation;
