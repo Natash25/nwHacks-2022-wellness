d606c0f5cc3bc65d0dceb3b749b50deee358af0e
Natash25 <natashacarson2509@gmail.com>
2022-01-16 01:00:27 -0800

    add the video components

---
+++ src/twilio-video-react-hooks/server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twilio: {
+    accountSid: 'AC9b7a96f60382e9c6c0e00ce4771200ac',
+    apiKey: 'SK4c8b4768fd9cb2282da33902a014a404',
+    apiSecret: 'pY6txbj5Vag0iapXEYzWjd4ASYp1M5P3'
+  }
+};
